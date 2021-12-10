8261f39a04bd2e78e0a3656b33de18e1af4bc946
rainbowfieldholograph <somebleed@tutanota.com>
2021-12-11 08:53:34 +0900

    rmvd docker, added prisma

--- api/index.js
+++ api/index.js
@@ -1,19 +1,16 @@
 require('dotenv').config()
 const express = require('express')
-// const sequelize = require('./db.js')
 const cors = require('cors')
 const PORT = process.env.PORT || 4000
-// const router = require('./routes/index')
+const router = require('./routes/index')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-// app.use('/api', router)
+app.use('/api', router)
 
 const start = async () => {
   try {
-    // await sequelize.authenticate()
-    // await sequelize.sync()
     app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
   } catch (error) {
     console.log('Cant connect to database: ', error)
